a0e4efd8833a2f999b065cd9c49dcdfac7a3c5a6
leidra <165079500+leidraisyana@users.noreply.github.com>
2024-03-27 02:11:42 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-cba9485f-d517-420a-a4bb-25e2586f23d0/)
 
 
 echo "$curl_output" >> Update.tsx
